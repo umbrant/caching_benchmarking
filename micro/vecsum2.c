b74b87717bb3c6c7644b8c24dc7720a2c2a79c2e
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-03-06 13:14:34 -0800

    vecsum2: add SSE intrinsics
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- micro/vecsum2.c
+++ micro/vecsum2.c
@@ -14,8 +14,11 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "immintrin.h"
+
 #define VECSUM_CHUNK_SIZE (8 * 1024 * 1024)
 #define NORMAL_READ_CHUNK_SIZE (8 * 1024)
+#define DOUBLES_PER_LOOP_ITER 8
 
 struct options {
 	// The path to read.
@@ -147,17 +150,69 @@ error:
 	return NULL;
 }
 
-static double vecsum(const double *buf, int byte_size)
+static int check_byte_size(int byte_size, const char *const str)
+{
+	if (byte_size % sizeof(double)) {
+		fprintf(stderr, "%s is not a multiple "
+			"of sizeof(double)\n", str);
+		return EINVAL;
+	}
+	if ((byte_size / sizeof(double)) % DOUBLES_PER_LOOP_ITER) {
+		fprintf(stderr, "The number of doubles contained in "
+			"%s is not a multiple of DOUBLES_PER_LOOP_ITER\n",
+			str);
+		return EINVAL;
+	}
+	return 0;
+}
+
+#ifdef SIMPLE_VECSUM
+
+static double vecsum(const double *buf, int num_doubles)
 {
 	int i, size;
 	double sum = 0.0;
-
-	size = byte_size / sizeof(double);
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < num_doubles; i++) {
 		sum += buf[i];
 	}
 	return sum;
 }
+
+#else
+
+static double vecsum(const double *buf, int num_doubles)
+{
+	int i;
+	double hi, lo;
+	__m128d x0 = _mm_set_pd(0.0,0.0);
+	__m128d x1 = _mm_set_pd(0.0,0.0);
+	__m128d x2 = _mm_set_pd(0.0,0.0);
+	__m128d x3 = _mm_set_pd(0.0,0.0);
+	__m128d sum0 = _mm_set_pd(0.0,0.0);
+	__m128d sum1 = _mm_set_pd(0.0,0.0);
+	__m128d sum2 = _mm_set_pd(0.0,0.0);
+	__m128d sum3 = _mm_set_pd(0.0,0.0);
+	for (i = 0; i < num_doubles; i+=DOUBLES_PER_LOOP_ITER) {
+		x0 = _mm_load_pd(buf + i + 0);
+		sum0 = _mm_add_pd(sum0, x0);
+		x1 = _mm_load_pd(buf + i + 0);
+		sum1 = _mm_add_pd(sum1, x1);
+		x2 = _mm_load_pd(buf + i + 0);
+		sum2 = _mm_add_pd(sum2, x2);
+		x3 = _mm_load_pd(buf + i + 0);
+		sum3 = _mm_add_pd(sum3, x3);
+	}
+	x0 = _mm_set_pd(0.0,0.0);
+	x0 = _mm_add_pd(x0, sum0);
+	x0 = _mm_add_pd(x0, sum1);
+	x0 = _mm_add_pd(x0, sum2);
+	x0 = _mm_add_pd(x0, sum3);
+	_mm_storeh_pd(&hi, x0);
+	_mm_storel_pd(&lo, x0);
+	return hi + lo;
+}
+
+#endif
 
 static int vecsum_zcr(struct test_data *tdata, const struct options *opts)
 {
@@ -200,7 +255,7 @@ static int vecsum_zcr(struct test_data *tdata, const struct options *opts)
 			goto done;
 		}
 		buf = hadoopRzBufferGet(rzbuf);
-		sum += vecsum(buf, VECSUM_CHUNK_SIZE);
+		sum += vecsum(buf, VECSUM_CHUNK_SIZE / sizeof(double));
 		hadoopRzBufferFree(tdata->file, rzbuf);
 	}
 	printf("finished zcr pass.  sum = %g\n", sum);
@@ -242,7 +297,7 @@ static int vecsum_normal(struct test_data *tdata, const struct options *opts)
 				"length %d\n", res);
 			goto done;
 		}
-		sum += vecsum(tdata->buf, NORMAL_READ_CHUNK_SIZE);
+		sum += vecsum(tdata->buf, NORMAL_READ_CHUNK_SIZE / sizeof(double));
 	}
 	printf("finished normal pass.  sum = %g\n", sum);
 	ret = 0;
@@ -257,6 +312,11 @@ int main(void)
 	struct options *opts = NULL;
 	struct test_data *tdata = NULL;
 
+	if (check_byte_size(VECSUM_CHUNK_SIZE, "VECSUM_CHUNK_SIZE") ||
+		check_byte_size(NORMAL_READ_CHUNK_SIZE,
+				"NORMAL_READ_CHUNK_SIZE")) {
+		goto done;
+	}
 	opts = options_create();
 	if (!opts)
 		goto done;
