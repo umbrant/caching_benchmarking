5e2e232e12e496f098f747ef3b0aa785338a61b6
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-03-07 13:43:19 -0800

    vecsum2: fix inner loop
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- micro/vecsum2.c
+++ micro/vecsum2.c
@@ -21,7 +21,7 @@
 #include "immintrin.h"
 
 #define VECSUM_CHUNK_SIZE (8 * 1024 * 1024)
-#define ZCR_READ_CHUNK_SIZE (1024 * 1024 * 4) // 8
+#define ZCR_READ_CHUNK_SIZE (1024 * 1024 * 8) // 8
 #define NORMAL_READ_CHUNK_SIZE (4 * 1024 * 1024)
 #define DOUBLES_PER_LOOP_ITER 8
 
@@ -285,12 +285,12 @@ static double vecsum(const struct options *opts,
 	__m128d sum3 = _mm_set_pd(0.0,0.0);
 	for (i = 0; i < num_doubles; i+=DOUBLES_PER_LOOP_ITER) {
 		x0 = _mm_load_pd(buf + i + 0);
+		x1 = _mm_load_pd(buf + i + 2);
+		x2 = _mm_load_pd(buf + i + 4);
+		x3 = _mm_load_pd(buf + i + 6);
 		sum0 = _mm_add_pd(sum0, x0);
-		x1 = _mm_load_pd(buf + i + 0);
 		sum1 = _mm_add_pd(sum1, x1);
-		x2 = _mm_load_pd(buf + i + 0);
 		sum2 = _mm_add_pd(sum2, x2);
-		x3 = _mm_load_pd(buf + i + 0);
 		sum3 = _mm_add_pd(sum3, x3);
 	}
 	x0 = _mm_set_pd(0.0,0.0);
