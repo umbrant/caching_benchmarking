54a908f29f9b08eaae677b352cbd8a8df9aa393b
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-03-09 21:14:20 -0700

    vecsum2: more efficient inner loop
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- micro/vecsum2.c
+++ micro/vecsum2.c
@@ -23,7 +23,7 @@
 #define VECSUM_CHUNK_SIZE (8 * 1024 * 1024)
 #define ZCR_READ_CHUNK_SIZE (1024 * 1024 * 8) // 8
 #define NORMAL_READ_CHUNK_SIZE (4 * 1024 * 1024)
-#define DOUBLES_PER_LOOP_ITER 8
+#define DOUBLES_PER_LOOP_ITER 16
 
 static double timespec_to_double(struct timespec *ts)
 {
@@ -188,6 +188,7 @@ static void test_data_free(struct test_data *tdata)
 		}
 		hdfsDisconnect(tdata->fs);
 	}
+	free(tdata);
 }
 
 static struct test_data *test_data_create(const struct options *opts)
@@ -291,27 +292,45 @@ static double vecsum(const struct options *opts,
 	__m128d x1 = _mm_set_pd(0.0,0.0);
 	__m128d x2 = _mm_set_pd(0.0,0.0);
 	__m128d x3 = _mm_set_pd(0.0,0.0);
+	__m128d x4 = _mm_set_pd(0.0,0.0);
+	__m128d x5 = _mm_set_pd(0.0,0.0);
+	__m128d x6 = _mm_set_pd(0.0,0.0);
+	__m128d x7 = _mm_set_pd(0.0,0.0);
 	__m128d sum0 = _mm_set_pd(0.0,0.0);
 	__m128d sum1 = _mm_set_pd(0.0,0.0);
 	__m128d sum2 = _mm_set_pd(0.0,0.0);
 	__m128d sum3 = _mm_set_pd(0.0,0.0);
+	__m128d sum4 = _mm_set_pd(0.0,0.0);
+	__m128d sum5 = _mm_set_pd(0.0,0.0);
+	__m128d sum6 = _mm_set_pd(0.0,0.0);
+	__m128d sum7 = _mm_set_pd(0.0,0.0);
 	for (i = 0; i < num_doubles; i+=DOUBLES_PER_LOOP_ITER) {
 		x0 = _mm_load_pd(buf + i + 0);
 		x1 = _mm_load_pd(buf + i + 2);
 		x2 = _mm_load_pd(buf + i + 4);
 		x3 = _mm_load_pd(buf + i + 6);
+		x4 = _mm_load_pd(buf + i + 8);
+		x5 = _mm_load_pd(buf + i + 10);
+		x6 = _mm_load_pd(buf + i + 12);
+		x7 = _mm_load_pd(buf + i + 14);
 		sum0 = _mm_add_pd(sum0, x0);
 		sum1 = _mm_add_pd(sum1, x1);
 		sum2 = _mm_add_pd(sum2, x2);
 		sum3 = _mm_add_pd(sum3, x3);
+		sum4 = _mm_add_pd(sum4, x4);
+		sum5 = _mm_add_pd(sum5, x5);
+		sum6 = _mm_add_pd(sum6, x6);
+		sum7 = _mm_add_pd(sum7, x7);
 	}
-	x0 = _mm_set_pd(0.0,0.0);
-	x0 = _mm_add_pd(x0, sum0);
-	x0 = _mm_add_pd(x0, sum1);
-	x0 = _mm_add_pd(x0, sum2);
-	x0 = _mm_add_pd(x0, sum3);
-	_mm_storeh_pd(&hi, x0);
-	_mm_storel_pd(&lo, x0);
+	x0 = _mm_add_pd(sum0, sum1);
+	x1 = _mm_add_pd(sum2, sum3);
+	x2 = _mm_add_pd(sum4, sum5);
+	x3 = _mm_add_pd(sum6, sum7);
+	x4 = _mm_add_pd(x0, x1);
+	x5 = _mm_add_pd(x2, x3);
+	x6 = _mm_add_pd(x4, x5);
+	_mm_storeh_pd(&hi, x6);
+	_mm_storel_pd(&lo, x6);
 	return hi + lo;
 }
 
