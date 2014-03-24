ecd306ad7c470accd16350964d17eeba62c3728b
Andrew Wang <andrew.wang@cloudera.com>
2014-03-24 13:57:53 -0700

    improve MR duration parser

--- scripts/mapreduce/compute_duration.sh
+++ scripts/mapreduce/compute_duration.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then echo "$0 <output from MR job>"; exit -1; fi
+if [ "$#" -ne 1 ]; then echo "$0 <output folder from MR job>"; exit -1; fi
+
+if [ ! -d $1 ]; then
+    echo "Expected path to be a folder"
+    exit -1
+fi
 
 MR=$1
 shift
 
-START=`grep "map 0" $MR | cut -d " " -f 2`
-END=`grep "reduce 100" $MR | cut -d " " -f 2`
+for f in `ls $MR`; do
+    FILE=`pwd`/$MR/$f
+    START=`grep "map 0" $FILE | cut -d " " -f 2 | head -1`
+    MAP_END=`grep "map 100" $FILE | cut -d " " -f 2 | head -1`
+    END=`grep "reduce 100" $FILE | cut -d " " -f 2 | head -1`
 
-START_TIMESTAMP=`date --date="$START" +%s`
-END_TIMESTAMP=`date --date="$END" +%s`
+    START_TIMESTAMP=`date --date="$START" +%s`
+    MAP_END_TIMESTAMP=`date --date="$MAP_END" +%s`
+    END_TIMESTAMP=`date --date="$END" +%s`
 
-echo "Seconds:" $(( $END_TIMESTAMP - $START_TIMESTAMP ))
+    echo -n "Map:" $(( $MAP_END_TIMESTAMP - $START_TIMESTAMP ))
+    echo -n ", Reduce:" $(( $END_TIMESTAMP - $MAP_END_TIMESTAMP ))
+    echo -n ", Total:" $(( $END_TIMESTAMP - $START_TIMESTAMP ))
+    echo
+done
