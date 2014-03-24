2ee0c995da110dbb7fadb66107fdda3ed8a7fa48
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-03-23 23:11:02 -0700

    add dewiki caching scripts

--- scripts/mapreduce/loop_mr.sh
+++ scripts/mapreduce/loop_mr.sh
@@ -4,7 +4,7 @@ if [ "$#" -ne 3 ]; then echo "$0 <grep/wc> <number of runs> <output folder>"; ex
 
 # Useful constants
 HIN="/wikipedia_snapshots/dewiki-20121117-pages-meta-current.xml"
-EXAMPLE_JAR="/home/cloudera/parcels/CDH-5.0.0-1.cdh5.0.0.p0.14/lib/hadoop-0.20-mapreduce/hadoop-examples-2.3.0-mr1-cdh5.0.0-SNAPSHOT.jar"
+EXAMPLE_JAR="/home/cloudera/parcels/CDH-5.0.0-1.cdh5.0.0.p0.36/lib/hadoop-0.20-mapreduce/hadoop-examples-2.3.0-mr1-cdh5.0.0-SNAPSHOT.jar"
 
 # Command specification
 
---
+++ scripts/mapreduce/uncache_dewiki.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo -u hdfs hdfs cacheadmin -removeDirectives /wikipedia_snapshots/dewiki-20121117-pages-meta-current.xml
